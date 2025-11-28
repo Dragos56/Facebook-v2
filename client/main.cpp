a8df7481d39e7d1e6a01991906ca2adbd7343db5
Dragos56 <dragosfron56@gmail.com>
2025-11-27 21:36:24 -0500

    first commit

---
+++ client/main.cpp
@@ -0,0 +1,73 @@
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_glfw.h"
+#include "imgui/backends/imgui_impl_opengl3.h"
+
+#include <GLFW/glfw3.h>
+
+#include "network.h"
+
+int main() {
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(800, 600, "ImGui Test", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    if (!connect_to_server("10.100.0.30", 5656)) {
+        printf("Nu m-am putut conecta la server!\n");
+    } else {
+        printf("Conectat cu succes!\n");
+    }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        static char command[100] = {0};
+        static char response[100] = {0};
+
+        ImGui::Begin("Client VirtualSoc");
+
+        ImGui::InputText("Comanda", command, IM_ARRAYSIZE(command));
+
+        if (ImGui::Button("Trimite")) {
+            send_message(command);
+
+            char buffer[100];
+            int n = receive_message(buffer);
+            if (n > 0) {
+                buffer[n] = '\0';
+                strcpy(response, buffer);
+            } else {
+                strcpy(response, "Eroare la primire.");
+            }
+        }
+
+        ImGui::Text("Raspuns server:");
+        ImGui::TextWrapped("%s", response);
+
+        ImGui::End();
+
+        ImGui::Render();
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwTerminate();
+
+    return 0;
+}
---
+++ client/network.h
@@ -0,0 +1,47 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+extern int errno;
+
+int sd;
+
+int connect_to_server(const char* ip, int port) {
+    struct sockaddr_in server;
+
+    if ((sd = socket (AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror ("socket() error");
+        return 0;
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr(ip);
+    server.sin_port = htons(port);
+
+    if (connect(sd, (struct sockaddr *) &server, sizeof(struct sockaddr)) == -1) {
+        perror("connect() error");
+        return 0;
+    }
+
+    return 1;
+}
+
+int send_message(const char* msg) {
+    return write(sd, msg, 100);
+}
+
+int receive_message(char* buffer) {
+    return read(sd, buffer, 100);
+}
+
+#endif
